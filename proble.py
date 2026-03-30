583f88c50ef6379623bd940597bb8577bfd6ee5f
Mushfiqur-Rahman-Apon <rahmanapon600@gmail.com>
2026-03-30 23:49:25 +0600

    Add files via upload
    problem solv

---
+++ proble.py
@@ -0,0 +1,16 @@
+# age = int(input("Your age:"))
+# if age >= 18:
+#     print("you age adult")
+# else:
+#     print("Your are not adult")    
+
+
+
+num = int(input("your number:"))
+
+
+if num % 2 == 0:
+   print("Enen Number")
+
+else:
+    print("Odd Number")
