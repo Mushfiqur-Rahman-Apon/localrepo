5ee1ad2b0ea85f819421ac4eeb0c6ee2bbf5e11d
Mushfiqur-Rahman-Apon <rahmanapon600@gmail.com>
2026-04-01 21:41:09 +0600

    Add files via upload
    problem solve

---
+++ problem5.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a> b:
+    if a>=c:
+        print(a, "is the largest")
+    else:
+        print(c, "is the leargest")
+
+
+else:
+    if b >= c:
+        print(b, "is the largest")
+        
+    else:
+        print(c, "is the largest")    
