d4ce26f4e18d2ba40963ba146695da670a0331ef
Mushfiqur-Rahman-Apon <rahmanapon600@gmail.com>
2026-04-11 19:46:30 +0600

    Add files via upload
    module 3 practice question

---
+++ break.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+    int i = -5;
+    while (i<3)
+
+    {
+        printf("hello\n");
+    }
+    
+    return 0;
+
+}
+    
---
+++ loop_with_condition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+    for(int i=1;i<=1;i++)
+
+    
+    {
+        printf("%d\n");
+    }
+
+    return 0;
+}
